4608c0ddc83dde979b2069e227cd5c2f578b5b7a
hrl-2024 <hrl@bu.edu>
2023-10-16 19:51:16 -0400

    Instrumented test for MainActivityTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.7.2")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test:rules:1.4.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ app/src/androidTest/java/com/example/launcheractivity/MainActivityTest.kt
@@ -0,0 +1,41 @@
+package com.example.launcheractivity
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeUp
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
+
+    @Test
+    fun showsHomeTextOnLaunch() {
+        onView(withId(R.id.homeActivity)).check(matches(withText("Home")))
+    }
+
+    @Test
+    fun swipeUpGoNorth() {
+        onView(withId(R.id.homeActivity)).perform(swipeUp())
+    }
+}
